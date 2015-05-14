700176dca206ab05c476d3e0c9da7197199e6b81
Kris Sankaran <krissankaran@DN0a22c058.SUNet>
2015-05-14 10:58:38 -0700

    Give roxygen comments for classes.

--- R/allClasses.R
+++ R/allClasses.R
@@ -1,26 +1,26 @@
 # spectra -----------------------------------------------------------------
-#' The S4 class for storing metabolomic information
-#'
+#'  @title The S4 class for storing metabolomic information
 #'  @description A matrix containing either raw spectra measurements or
 #'    processed peaks indicators or heights.
-#'
 #'  @name spectra-class
 #'  @exportClass spectra
 setClass("spectra", representation(peaks = "logical"),
          contains = "matrix")
 
 # null-unions -------------------------------------------------------------
-#' If a component of the phyloseq extend object is not present, we keep a NULL
-#' placeholder.
-#'
-#' @keywords internal
+#'  @title spectra S4 class with placeholder
+#'  @description If a component of the phyloseq extend object is not present, we keep a NULL
+#'    placeholder.
+#'  @keywords internal
+#'  @name spectraOrNull-class
+#'  @exportClass spectraOrNull
 setClassUnion("spectraOrNull", c("spectra", "NULL"))
 
 # phyloseq-extend ---------------------------------------------------------
-#' The high-level extended phyloseq object, including new slot types
-#'
+#'  @title The high-level extended phyloseq object, including new slot types
 #'  @slot spectra An object of class spectra, used for storing metabolomic
 #'    information
+#'  @name phyloseqExtend-class
 #'  @importClassesFrom phyloseq phyloseq
 #'  @exportClass phyloseqExtend
 setClass("phyloseqExtend",
