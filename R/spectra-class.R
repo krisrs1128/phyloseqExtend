506df1c4d777d781b4a81dca2b34f303a2f1965c
Kris Sankaran <krissankaran@DN0a22c670.SUNet>
2015-05-17 13:43:42 -0700

    Let user input spectra as a data.frame, remove extra space from roxygen comments.

--- R/spectra-class.R
+++ R/spectra-class.R
@@ -1,14 +1,17 @@
 # build-spectra-object ----------------------------------------------------
-#'  @title Build or access an object of class spectra
-#'  @param spectra_matrix An object of class matrix, containing the spectral
-#'    samples as rows.
-#'  @param peaks A logical indicating whether the matrix columns corresponds to
-#'    wavelengths (FALSE, for raw spectra) or just peaks (TRUE, for spectra
-#'    that have been processed). Defaults to FALSE.
-#'  @return spectra An object of class spectra, containing the input spectra
-#'    matrix as the main data component.
-#'  @docType methods
-#'  @export
+#' @title Build or access an object of class spectra
+#'
+#' @param spectra_matrix An object of class matrix, containing the spectral
+#'  samples as rows.
+#' @param peaks A logical indicating whether the matrix columns corresponds to
+#'  wavelengths (FALSE, for raw spectra) or just peaks (TRUE, for spectra
+#'  that have been processed). Defaults to FALSE.
+#'
+#' @return spectra An object of class spectra, containing the input spectra
+#' matrix as the main data component.
+#'
+#' @docType methods
+#' @export
 
 setGeneric("spectra", function(object) {
   standardGeneric("spectra")
@@ -18,23 +21,28 @@ setGeneric("spectra", function(object) {
 setMethod("spectra", "phyloseqExtend", function(object) {
   spectra_object <- slot(object, "spectra")
   if(is.null(spectra_object)) {
-    warning("spectra slot is null.")
+    warning("No spectra available.")
   }
   return (spectra_object)
 })
 
 # If a matrix is input, create a new spectra object from that matrix
 setMethod("spectra", "matrix", function(object) {
-  return (new("spectra", object))
+  new("spectra", object)
 })
 
+# If a data.frame or data.table is input, convert to a matrix
+setMethod("spectra", "data.frame", function(object) {
+  spectra(as(object, "matrix"))
+})
 
 # call-peaks --------------------------------------------------------------
-#'  @title Extract aligned peaks from raw spectra
-#'  @param spectra A spectra object containing raw spectra reads
-#'  @return spectra The original spectra object, updated so that the data
-#'    object includes a binary indicator for peaks
-#'  @importFrom speaq detectSpecPeaks
+#' @title Extract aligned peaks from raw spectra
+#'
+#' @param spectra A spectra object containing raw spectra reads
+#' @return spectra The original spectra object, updated so that the data
+#'  object includes a binary indicator for peaks
+#' @importFrom speaq detectSpecPeaks
 extract_peaks <- function(spectra, ...) {
   if(spectra@peaks) {
     warning("spectra peaks are already extracted")
