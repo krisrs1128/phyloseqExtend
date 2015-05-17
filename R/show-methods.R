3190b3ef767c695b2e43580d8d2ec91203942cee
Kris Sankaran <krissankaran@DN0a22c670.SUNet>
2015-05-17 13:49:56 -0700

    Just call higher level phyloseq "show" method, rather than copying over for phyloseqExtend.

--- R/show-methods.R
+++ R/show-methods.R
@@ -4,37 +4,9 @@
 #' @export
 #' @docType methods
 setMethod("show", "phyloseqExtend", function(object){
-  cat("phyloseqExtend-class experiment-level object", fill=TRUE)
-  # print otu_table (always there).
-  cat(paste("otu_table()   OTU Table:         [ ", ntaxa(otu_table(object)), " taxa and ",
-            nsamples(otu_table(object)), " samples ]", sep = ""), fill = TRUE)
-  # print Sample Data if there
-  if(!is.null(sample_data(object, FALSE))){
-    cat(paste("sample_data() Sample Data:       [ ", dim(sample_data(object))[1], " samples by ",
-              dim(sample_data(object))[2],
-              " sample variables ]", sep = ""), fill = TRUE)
-  }
-
-  # print tax Tab if there
-  if(!is.null(tax_table(object, FALSE))){
-    cat(paste("tax_table()   Taxonomy Table:    [ ", dim(tax_table(object))[1], " taxa by ",
-              dim(tax_table(object))[2],
-              " taxonomic ranks ]", sep = ""), fill = TRUE)
-  }
-
-  # print tree if there
-  if(!is.null(phy_tree(object, FALSE))){
-    cat(paste("phy_tree()    Phylogenetic Tree: [ ", ntaxa(phy_tree(object)), " tips and ",
-              phy_tree(object)$Nnode,
-              " internal nodes ]", sep = ""),
-        fill = TRUE
-    )
-  }
-
-  # print refseq summary if there
-  if(!is.null(refseq(object, FALSE))){
-    cat(paste("refseq()      ", class(refseq(object))[1], ":      [ ", ntaxa(refseq(object)), " reference sequences ]", sep = ""), fill=TRUE)
-  }
+  # Print usual phyloseq tables
+  signature(object = "phyloseq")
+  callNextMethod()
 
   # print spectra summary if there
   spec_obj <- spectra(object)
