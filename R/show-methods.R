93b343e8939251830ccd143ef72ad6dbd4f24e4b
Kris Sankaran <krissankaran@DN0a22c74a.SUNet>
2015-06-09 12:03:57 -0700

    Update show methods to print all the matrices belonging to spectra.

--- R/show-methods.R
+++ R/show-methods.R
@@ -11,7 +11,9 @@ setMethod("show", "phyloseqExtend", function(object){
   # print spectra summary if there
   spec_obj <- spectra(object)
   if(!is.null(spec_obj)) {
-    cat(paste("spectra()     ", class(spec_obj)[1], ":           [ ", nrow(spec_obj), " samples by ", ncol(spec_obj), " indices ]", sep = ""), fill = TRUE)
+    specmat_string <- paste0("\t\t\t specmat [ ", nrow(spec_obj@specmat), " samples by ", ncol(spec_obj@specmat), " indices]\n")
+    peakmat_string <- paste0("\t\t\t peakmat [ ", nrow(spec_obj@peakmat), " samples by ", ncol (spec_obj@peakmat), " peaks]\n")
+    adjmat_string <- paste0("\t\t\t adjmat  [ ", nrow(spec_obj@adjmat), " samples by ", ncol(spec_obj@adjmat), " samples]\n")
+    cat(paste0("spectra()     Spectra: \n", specmat_string, peakmat_string, adjmat_string), fill = TRUE)
   }
-
 })
