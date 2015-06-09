f51fa7deaf58ca84ad13bba08aea311d2168ec7e
Kris Sankaran <krissankaran@DN0a22c74a.SUNet>
2015-06-09 15:05:05 -0700

    Minor changes to show for spectra mat.

--- R/show-methods.R
+++ R/show-methods.R
@@ -3,14 +3,14 @@
 #'
 #' @export
 #' @docType methods
-setMethod("show", "phyloseqExtend", function(object){
-  # Print usual phyloseq tables
-  signature(object = "phyloseq")
-  callNextMethod()
+setMethod("show", "phyloseqExtend", function(object) {
+   # Print usual phyloseq tables
+   signature(object = "phyloseq")
+   callNextMethod()
 
-  # print spectra summary if there
-  spec_obj <- spectra(object)
-  if(!is.null(spec_obj)) {
+    # print spectra summary if there
+   spec_obj <- spectra(object)
+   if(!is.null(spec_obj)) {
     specmat_string <- paste0("\t\t\t specmat [ ", nrow(spec_obj@specmat), " samples by ", ncol(spec_obj@specmat), " indices]\n")
     peakmat_string <- paste0("\t\t\t peakmat [ ", nrow(spec_obj@peakmat), " samples by ", ncol (spec_obj@peakmat), " peaks]\n")
     adjmat_string <- paste0("\t\t\t adjmat  [ ", nrow(spec_obj@adjmat), " samples by ", ncol(spec_obj@adjmat), " samples]\n")
