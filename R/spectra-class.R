b34446b11192e922533539a5890098abefc07635
Kris Sankaran <krissankaran@DN0a22c74a.SUNet>
2015-06-09 13:31:23 -0700

    Add methods for pruning spectra based on row names.

--- R/spectra-class.R
+++ R/spectra-class.R
@@ -22,9 +22,34 @@ setMethod("spectra", "phyloseqExtend", function(object) {
   return (spectra_object)
 })
 
+#' @title Add default row and column names for a spectra object
+add_dim_names <- function(object) {
+  if(is.null(rownames(object@specmat)) & nrow(object@specmat) > 0) {
+    rownames(object@specmat) <- paste0("sa", 1:nrow(object@specmat))
+  }
+  if(is.null(colnames(object@specmat)) & ncol(object@specmat) > 0) {
+    colnames(object@specmat)  <- paste0("ind", 1:ncol(object@specmat))
+  }
+  if(is.null(rownames(object@peakmat)) & nrow(object@peakmat) > 0) {
+    rownames(object@peakmat) <- paste0("sa", 1:nrow(object@peakmat))
+  }
+  if(is.null(colnames(object@peakmat)) & ncol(object@peakmat) > 0) {
+    rownames(object@peakmat) <- paste0("pk", 1:ncol(object@peakmat))
+  }
+  if(is.null(rownames(object@adjmat)) & nrow(object@adjmat) > 0) {
+   rownames(object@adjmat) <- paste0("sa", 1:nrow(object@adjmat))
+  }
+  if(is.null(rownames(object@adjmat)) & ncol(object@adjmat) > 0) {
+   rownames(object@adjmat) <- paste0("sa", 1:nrow(object@adjmat))
+  }
+  return (object)
+}
+
 # If a single matrix is input, assume that matrix is the specmat
 setMethod("spectra", "matrix", function(object) {
-  new("spectra", specmat = object)
+  spectra_obj <- new("spectra", specmat = object)
+  spectra_obj <- add_dim_names(spectra_obj)
+  return (spectra_obj)
 })
 
 # If a data.frame or data.table is input, convert to a matrix and
@@ -38,6 +63,8 @@ setMethod("spectra", "list", function(object) {
   new("spectra", specmat = as.matrix(object[["specmat"]]),
       peakmat = as.matrix(object[["peakmat"]]),
       adjmat = as.matrix(object[["adjmat"]]))
+  spectra_obj <- add_dim_names(spectra_obj)
+  return (spectra_obj)
 })
 
 # validity ----------------------------------------------------------------
@@ -50,10 +77,43 @@ setValidity("spectra",
                 if(nspec != npeak) {
                   "Number of spectra samples not equal to number of peak samples."
                 }
+                if(rownames(object@specmat) != rownames(object@peakmat)) {
+                  "Spectra row names are inconsistent"
+                }
               } else {
                 TRUE
               }
             })
+
+# sample-names ------------------------------------------------------------
+
+setMethod("sample_names", "spectra", function(physeq) {
+  # Have to use argument physeq for consistency for phyloseq, but it
+  # actually expects a spectra object as its argument.
+  return (rownames(physeq@specmat))
+})
+
+# prune-samples -----------------------------------------------------------
+
+#' @title Subset the samples of an object of class spectra
+#'
+#' @param samples_ix A vector of integers giving the rows of the spectra
+#'  matrices to include.
+#' @param spectra An object of class spectra, whose matrices we want to filter.
+subset_samples_spectra <- function(samples_ix, spectra) {
+  if(nrow(spectra@specmat) > 0) spectra@specmat <- spectra@specmat[samples_ix, ]
+  if(nrow(spectra@peakmat) > 0) spectra@peakmat <- spectra@peakmat[samples_ix, ]
+  if(nrow(spectra@adjmat) > 0) spectra@adjmat <- spectra@adjmat[samples_ix, ]
+  return (spectra)
+}
+
+#' @title Define a prune_samples methods for spectra class
+#'
+setMethod("prune_samples", signature("character", "spectra"), function(samples, x) {
+  samples_ix <- which(sample_names(x) %in% samples)
+  x <- subset_samples_spectra(samples_ix, x)
+  return (x)
+})
 
 # call-peaks --------------------------------------------------------------
 #' @title Wrapper for speaq::detectSpecPeaks
